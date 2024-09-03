39560a0533d251461cc3c2ab604ff06fe7c0fea5
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2024-09-03 12:43:23 +0200

    feat(updater): move github config to accolades (#6)
    Move from Microsoft test repository (Powertoys) to accoladeshq
    
    +semver: feature

--- src/Updater/Constants.cs
+++ src/Updater/Constants.cs
@@ -3,7 +3,7 @@
 internal class Constants
 {
     // ########################## GitHub ##########################
-    public const string GitHubOrganization = "microsoft";
-    public const string GitHubProject = "PowerToys";
+    public const string GitHubOrganization = "accoladeshq";
+    public const string GitHubProject = "Brann";
     public const string GitHubAppContentType = "application/x-msdownload";
 }
