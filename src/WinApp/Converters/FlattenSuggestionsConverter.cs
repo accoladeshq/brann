f9635025d066621656b6f5beabfed7827d4e290b
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2024-09-11 17:42:47 +0200

    fix(palette): rework suggestions to remove code from viewmodel

---
+++ src/WinApp/Converters/FlattenSuggestionsConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using Accolades.Brann.Core;
+using Accolades.Brann.Plugins;
+using Avalonia.Data.Converters;
+
+namespace Accolades.Brann.Converters;
+
+public class FlattenSuggestionsConverter : IValueConverter
+{
+    /// <summary>
+    /// Flatten a <see cref="ISuggestions"/> into a <see cref="IEnumerable{ISuggestion}"/>
+    /// </summary>
+    /// <param name="value">The list to flatten.</param>
+    /// <param name="targetType">The target type.</param>
+    /// <param name="parameter">The parameter.</param>
+    /// <param name="culture">The culture.</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">If the value is not a <see cref="ISuggestions"/></exception>
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not ISuggestions suggestions)
+        {
+            throw new ArgumentException();
+        }
+        
+        var list = new List<ISuggestion>();
+        
+        foreach (var suggestionCategory in suggestions)
+        {
+            list.Add(suggestionCategory);
+            list.AddRange(suggestionCategory);
+        }
+
+        return list;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/WinApp/ViewModels/PaletteViewModel.cs
+++ src/WinApp/ViewModels/PaletteViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using System.Reactive;
-using System.Reactive.Linq;
 using System.Windows.Input;
 using Accolades.Brann.Avalonia;
 using Accolades.Brann.Core;
 using Accolades.Brann.Models;
-using Accolades.Brann.Plugins;
-using DynamicData.Binding;
 using ReactiveUI;
 using Splat;
 
@@ -23,27 +20,7 @@ public class PaletteViewModel : ViewModelBase
         SuggestionProvider = Locator.Current.GetRequiredService<ISuggestionProvider>();
         _dialogService = Locator.Current.GetRequiredService<IDialogService>();
         _displaySettingsCommand = ReactiveCommand.CreateFromTask(DisplaySettings);
-        
-        this.WhenValueChanged(vm => vm.SuggestionProvider.Suggestions)
-            .Where(s => s is not null)
-            .Select(FlattenSuggestionsFromProvider!)
-            .ToProperty(this, vm => vm.Suggestions, out _suggestions);
     }
-
-    /// <summary>
-    /// Gets or sets the search term.
-    /// </summary>
-    public string SearchTerm
-    {
-        get => SuggestionProvider.SearchTerm;
-        set => SuggestionProvider.SearchTerm = value;
-    }
-    
-    private readonly ObservableAsPropertyHelper<IEnumerable<ISuggestion>> _suggestions;
-    /// <summary>
-    /// Gets the suggestions.
-    /// </summary>
-    public IEnumerable<ISuggestion> Suggestions => _suggestions.Value;
     
     private readonly ReactiveCommand<Unit, Unit> _displaySettingsCommand;
     /// <summary>
@@ -51,7 +28,10 @@ public class PaletteViewModel : ViewModelBase
     /// </summary>
     public ICommand DisplaySettingsCommand => _displaySettingsCommand;
     
-    private ISuggestionProvider SuggestionProvider { get; }
+    /// <summary>
+    /// Gets the suggestion provider.
+    /// </summary>
+    public ISuggestionProvider SuggestionProvider { get; }
     
     /// <summary>
     /// Display settings view.
@@ -62,18 +42,5 @@ public class PaletteViewModel : ViewModelBase
     {
         var r = await _dialogService.Open<SettingsViewModel, Unit>();
         return r;
-    }
-
-    private static IEnumerable<ISuggestion> FlattenSuggestionsFromProvider(ISuggestions suggestions)
-    {
-        var list = new List<ISuggestion>();
-        
-        foreach (var suggestionCategory in suggestions)
-        {
-            list.Add(suggestionCategory);
-            list.AddRange(suggestionCategory);
-        }
-
-        return list;
     }
 }
